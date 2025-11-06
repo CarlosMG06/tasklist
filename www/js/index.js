bf3f2b3de4be35e78b7a2e7756e81cfc7b2359d6
CarlosMG06 <220267058+CarlosMG06@users.noreply.github.com>
2025-11-06 19:15:41 +0100

    content-security-policy no inlines, separat arxius

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,104 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+$(document).ready(function() {
+    $("#afegeix").click(afegeixDialog);
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+    const localTasks = localStorage.getItem('tasques');
+    if (localTasks !== null) {
+        const taskArray = JSON.parse(localTasks);
+        if (taskArray.length > 0) {
+            // Amaga el missatge de cap tasca
+            $('#capTasca').hide();
+        }
+        for (let i = 0; i < taskArray.length; i++) {
+            afegeix(taskArray[i]);
+        }
+    } else {
+        localStorage.setItem('tasques', JSON.stringify([]));
+    }
+});
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+function afegeix(taskname) {
+    const elem = $(
+        '<div class="tasca">' + 
+            '<span class="nom">'+taskname+'</span>' +
+            '<button class="edita">Edita</button>' +
+            '<button class="elimina">&times;</button>' +
+        '</div>');
+    $(".edita", elem).click(edita);
+    $(".elimina", elem).click(elimina);
+    $('#list').append(elem);
+}
+function afegeixDialog() {
+    $("#dialog").dialog({
+        resizable: false,
+        height: "auto",
+        width: 400,
+        modal: true,
+        buttons: {
+            "Afegir tasca": function() {
+                const taskname = $('input').val();
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+                const taskArray = JSON.parse(localStorage.getItem('tasques'));
+                taskArray.push(taskname);
+                localStorage.setItem('tasques', JSON.stringify(taskArray));
+                
+                afegeix(taskname);
+                
+                // Amaga el missatge de cap tasca
+                if ($('#capTasca').is(':visible')) {
+                    $('#capTasca').hide();
+                }
+
+                $('input').val("");
+                $(this).dialog("close");                        
+            },
+            Cancel: function() {
+            $('input').val("");
+            $(this).dialog("close");
+            }
+        }
+    });  
+}
+function elimina(event) {
+    var caller = event.target || event.srcElement;
+    $(caller).parent().remove();
+
+    const taskname = $(caller).parent().find('.nom').text();
+    const taskArray = JSON.parse(localStorage.getItem('tasques'));
+    const index = taskArray.indexOf(taskname);
+    taskArray.splice(index, 1);
+    localStorage.setItem('tasques', JSON.stringify(taskArray));
+    
+    // Mostra el missatge de cap tasca si la llista està buida
+    if ($('#list').children().length === 0) {
+        $('#capTasca').show();
+    }
+}
+function edita(event) {
+    var caller = event.target || event.srcElement;
+    $("#dialog").dialog({
+        resizable: false,
+        height: "auto",
+        width: 400,
+        modal: true,
+        buttons: {
+            "Editar tasca": function() {
+                const taskname = $('input').val();
+
+                const oldTaskName = $(caller).parent().find('.nom').text();
+                const taskArray = JSON.parse(localStorage.getItem('tasques'));
+                const index = taskArray.indexOf(oldTaskName);
+                taskArray[index] = taskname;
+                localStorage.setItem('tasques', JSON.stringify(taskArray));
+                
+                $(caller).parent().find('.nom').text(taskname);
+
+                $('input').val("");
+                $(this).dialog("close");
+            },
+            Cancel: function() {
+            $('input').val("");
+            $(this).dialog("close");
+            }
+        }
+    });
 }
